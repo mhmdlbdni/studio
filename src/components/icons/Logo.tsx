77fee8eb78081469e5623d8fd6f792fd22e24d45
لتعلم <mhbaltlm3@gmail.com>
2025-06-23 22:11:46 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Logo className="h

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg {...props} width="65" height="54" viewBox="0 0 65 54" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M47.7831 23.3333C50.0465 24.5215 51.1782 25.1156 51.8468 26.046C52.5155 26.9764 52.6687 28.0934 52.3624 30.2222L51.8333 33.7778C51.5544 35.7111 51.4149 36.6778 50.8351 37.3889C50.2553 38.1 49.3481 38.3333 47.5338 38.8L17.4662 38.8C15.6519 38.3333 14.7447 38.1 14.1649 37.3889C13.5851 36.6778 13.4456 35.7111 13.1667 33.7778L12.6376 30.2222C12.3313 28.0934 12.4845 26.9764 13.1532 26.046C13.8218 25.1156 14.9535 24.5215 17.2169 23.3333L29.1304 17.5C30.4076 16.8778 31.0462 16.5667 31.7516 16.5147C32.457 16.4627 33.1499 16.6766 34.2356 17.1044L47.7831 23.3333Z" stroke="currentColor" strokeWidth="3" strokeLinejoin="round"/>
-        <path d="M32.5 4V16.6667" stroke="currentColor" strokeWidth="3" strokeLinecap="round"/>
-        <path d="M57.6666 43.3333C60.2139 43.3333 62.1133 42.4889 63.1666 40.8333" stroke="currentColor" strokeWidth="3" strokeLinecap="round"/>
-        <path d="M7.33337 43.3333C4.78611 43.3333 2.88673 42.4889 1.83337 40.8333" stroke="currentColor" strokeWidth="3" strokeLinecap="round"/>
-        <path d="M32.5 38.6666V50" stroke="currentColor" strokeWidth="3" strokeLinecap="round"/>
+    <svg {...props} width="64" height="64" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12 21V3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M21 6H3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M5 9C5 9 2 11.5 2 14.5C2 17.5 5 20 5 20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M5 20C5 20 8 17.5 8 14.5C8 11.5 5 9 5 9Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M19 9C19 9 16 11.5 16 14.5C16 17.5 19 20 19 20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M19 20C19 20 22 17.5 22 14.5C22 11.5 19 9 19 9Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
 );
