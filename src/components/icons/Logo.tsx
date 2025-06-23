42334985d2b0148d959d1bf45d6c068909b62de6
لتعلم <mhbaltlm3@gmail.com>
2025-06-23 22:21:35 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Logo className="h

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
     <svg {...props} width="64" height="64" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M3 6H21" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        <path d="M12 6V21" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        <path d="M6 12C6 15.3137 8.68629 18 12 18C15.3137 18 18 15.3137 18 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        <path d="M6 6L9 3H15L18 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M5 7h14" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
+        <path d="M12 7v12" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
+        <path d="M9 21h6" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
+        <path d="M5 7a4 4 0 0 0-4 4v0a4 4 0 0 0 4 4h0" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
+        <path d="M19 7a4 4 0 0 1 4 4v0a4 4 0 0 1-4 4h0" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
     </svg>
 );
