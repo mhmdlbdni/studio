aabc9df5a4f829681769446ab0f17bf82f01e1fa
لتعلم <mhbaltlm3@gmail.com>
2025-06-23 16:48:40 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.button

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -52,4 +52,11 @@ export const CURRENCIES = [
     { value: 'SAR', label: 'ريال سعودي (SAR)' },
     { value: 'USD', label: 'دولار أمريكي (USD)' },
     { value: 'EUR', label: 'يورو (EUR)' },
+    { value: 'AED', label: 'درهم إماراتي (AED)' },
+    { value: 'EGP', label: 'جنيه مصري (EGP)' },
+    { value: 'QAR', label: 'ريال قطري (QAR)' },
+    { value: 'KWD', label: 'دينار كويتي (KWD)' },
+    { value: 'BHD', label: 'دينار بحريني (BHD)' },
+    { value: 'OMR', label: 'ريال عماني (OMR)' },
+    { value: 'GBP', label: 'جنيه استرليني (GBP)' },
 ];
